ac9e63e18ea4b77c1827a60c0fed9cfc665d585d
Tommy12.Lin <tommy12.lin@advantech.com.tw>
2022-09-16 10:49:02 +0800

    temp

--- cinema_seat_allocation1386/Solution.java
+++ cinema_seat_allocation1386/Solution.java
@@ -1,18 +1,27 @@
 package cinema_seat_allocation1386;
 
+import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.Properties;
 
+
 class Solution {
     public static void main(String[] args) {
-        try{
-            int[][] reserveSeats;
-            Properties properties = new Properties();
-            InputStream inputStream = Solution.class.getResourceAsStream("input.properties");
-            properties.load(inputStream);
-            String a = inputStream.toString();
-//            int result = maxNumberOfFamilies(1000000000, reserveSeats);
-            System.out.println("a");
+        try {
+//            InputStream input = new FileInputStream("input.properties")
+//            Properties prop = new Properties();
+
+            // load a properties file
+//            prop.load(input);
+
+//            int[][] reserveSeats= {{4,3},{1,4},{4,6},{1,7}};
+
+            int[][] reserveSeats= 
+            {{2,10},{3,7},{1,5},{1,10},{1,1},{2,5},{1,4},{3,5},{1,7},{3,1},{3,3},{3,4},{1,9},{2,1},{3,2},{1,3},{2,8},{2,2},{1,6},{2,4},{3,6},{1,8},{3,8}};
+            
+            
+            int result = maxNumberOfFamilies(3, reserveSeats);
+            System.out.println(result);
         }catch (Exception ex){
             System.out.println(ex);
         }
@@ -20,46 +29,56 @@ class Solution {
 
     public static int maxNumberOfFamilies(int n, int[][] reservedSeats) {
         int numOfFourPersonSeats = 0;
-        int[][] availableFourPersonSeats = new int[n][3];
+        byte[] availableFourPersonSeats = new byte[n];
 
         for (int i = 0; i < n; i++) {
-            availableFourPersonSeats[i][0] = 1;
-            availableFourPersonSeats[i][1] = 1;
-            availableFourPersonSeats[i][2] = 1;
+            availableFourPersonSeats[i] = 7;
         }
 
         for (int i = 0; i < reservedSeats.length; i++) {
             int[] reserveSeat = reservedSeats[i];
             int row = reserveSeat[0] - 1;
             int col = reserveSeat[1] - 1;
+            if(availableFourPersonSeats[row] == 0) continue;
             switch (col) {
                 case 1:
                 case 2:
-                    availableFourPersonSeats[row][0] = 0;
+                    if(availableFourPersonSeats[row] != 3 && availableFourPersonSeats[row] != 2 && availableFourPersonSeats[row] != 1){
+                        availableFourPersonSeats[row] = (byte) (availableFourPersonSeats[row]-4);
+                    }
                     break;
                 case 3:
                 case 4:
-                    availableFourPersonSeats[row][0] = 0;
-                    availableFourPersonSeats[row][1] = 0;
+                    if(availableFourPersonSeats[row] != 3 && availableFourPersonSeats[row] != 2 && availableFourPersonSeats[row] != 1){
+                        availableFourPersonSeats[row] = (byte) (availableFourPersonSeats[row]-4);
+                    }
+                    if(availableFourPersonSeats[row] != 4 && availableFourPersonSeats[row] != 5 && availableFourPersonSeats[row] != 1 && availableFourPersonSeats[row] != 0){
+                        availableFourPersonSeats[row] = (byte) (availableFourPersonSeats[row]-2);
+                    }
                     break;
                 case 5:
                 case 6:
-                    availableFourPersonSeats[row][1] = 0;
-                    availableFourPersonSeats[row][2] = 0;
+                    if(availableFourPersonSeats[row] != 4 && availableFourPersonSeats[row] != 5 && availableFourPersonSeats[row] != 1){
+                        availableFourPersonSeats[row] = (byte) (availableFourPersonSeats[row]-2);
+                    }
+                    if(availableFourPersonSeats[row] != 6 && availableFourPersonSeats[row] != 4 && availableFourPersonSeats[row] != 2 && availableFourPersonSeats[row] != 0){
+                        availableFourPersonSeats[row] = (byte) (availableFourPersonSeats[row]-1);
+                    }
                     break;
                 case 7:
                 case 8:
-                    availableFourPersonSeats[row][2] = 0;
+                    if(availableFourPersonSeats[row] != 6 && availableFourPersonSeats[row] != 4 && availableFourPersonSeats[row] != 2){
+                        availableFourPersonSeats[row] = (byte) (availableFourPersonSeats[row]-1);
+                    }
                     break;
             }
         }
 
         for (int i = 0; i < n; i++) {
-            if (availableFourPersonSeats[i][0] == 1 || availableFourPersonSeats[i][2] == 1) {
-                availableFourPersonSeats[i][1] = 0;
-            }
-            for (int j = 0; j < 3; j++) {
-                numOfFourPersonSeats += availableFourPersonSeats[i][j];
+            if (availableFourPersonSeats[i] == 7) {
+                numOfFourPersonSeats += 2;
+            }else if(availableFourPersonSeats[i] != 0){
+                numOfFourPersonSeats += 1;
             }
         }
 
