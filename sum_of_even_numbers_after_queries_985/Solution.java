5a33713b29fdf72ce8f2c7df90338ffb43536004
Tommy12.Lin <tommy12.lin@advantech.com.tw>
2022-09-22 10:16:48 +0800

    985_complete

--- sum_of_even_numbers_after_queries_985/Solution.java
+++ sum_of_even_numbers_after_queries_985/Solution.java
@@ -11,16 +11,37 @@ public class Solution {
     public static int[] sumEvenAfterQueries(int[] nums, int[][] queries) {
         int[] answers = new int[queries.length];
         int queryRound = 0;
+
+        int sumOfQuery = 0;
+        boolean[] numsBeSumUp = new boolean[nums.length];
+
+        for(int i=0; i<nums.length; i++){
+            int num = nums[i];
+            if(num % 2 == 0){
+                numsBeSumUp[i] = true;
+                sumOfQuery += num;
+            }
+        }
+
         for(int[] query : queries){
-            int sumOfQuery = 0;
             int value = query[0];
             int index = query[1];
-            nums[index] = nums[index] + value;
-            for(int num : nums){
-                if(num % 2 == 0){
-                    sumOfQuery += num;
+
+            if((nums[index] + value) % 2 == 0){
+                if(numsBeSumUp[index]){
+                    sumOfQuery += value;
+                }else{
+                    sumOfQuery += nums[index] + value;
+                    numsBeSumUp[index] = true;
+                }
+            }else{
+                if(numsBeSumUp[index]){
+                    sumOfQuery -= nums[index];
+                    numsBeSumUp[index] = false;
                 }
             }
+            nums[index] = nums[index] + value;
+
             answers[queryRound++] = sumOfQuery;
         }
 
