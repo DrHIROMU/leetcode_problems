2992cb5938ef4c9550aeb3c30f380e4a34838bb1
Tommy12.Lin <tommy12.lin@advantech.com.tw>
2022-10-26 10:36:18 +0800

    9_palindrome_number_compelte

--- easy/palindrome_number_9/Solution.java
+++ easy/palindrome_number_9/Solution.java
@@ -1,4 +1,30 @@
 package easy.palindrome_number_9;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Solution {
+    public static void main(String[] args){
+        isPalindrome(313);
+    }
+
+    public static boolean isPalindrome(int x) {
+        List<Integer> numOfDigits = new ArrayList<>();
+        Integer palindromeNum = 0;
+        Integer inputNum = x;
+        while (x > 0) {
+            int numOfDigit = x % 10;
+            x = x / 10;
+            numOfDigits.add(numOfDigit);
+        }
+        for(Integer num : numOfDigits){
+            palindromeNum = palindromeNum*10 + num;
+        }
+        if(!inputNum.equals(palindromeNum)){
+           return false;
+        }
+
+        return true;
+    }
 }
