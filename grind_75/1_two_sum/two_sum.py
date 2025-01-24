b60c495f39545ed69decb4bf59d42177ae01a989
Lin Kung Hung <drhiromu@gmail.com>
2025-01-24 11:28:52 +0800

    Grind75 1. Two sum

---
+++ grind_75/1_two_sum/two_sum.py
@@ -0,0 +1,19 @@
+#https://leetcode.com/problems/two-sum/description/
+# Use a dictionary to store the difference between the target and the current number and the index of the current number.
+class solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        diff = {}
+        for index, num in enumerate(nums):
+            if num in diff:
+                return [diff[num], index]
+            diff[target - num] = index        
+        return []
+
+def main():
+    nums = [2, 7, 11, 15]
+    target = 9
+    s = solution()
+    print(s.twoSum(nums, target))
+
+if __name__ == '__main__':
+    main()
