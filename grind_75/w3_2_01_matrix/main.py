98e232d1784df47c2245370de80e37f8f48cbcd7
Tommy12.Lin <tommy12.lin@advantech.com.tw>
2025-04-08 13:11:07 +0800

    grind 75, 01 matrix. fast version

--- grind_75/w3_2_01_matrix/main.py
+++ grind_75/w3_2_01_matrix/main.py
@@ -1,70 +1,32 @@
-# 要用Multi-source BFS才會快, 把所有0作為起點放進queue, 逐步往外擴展
+# 要用Multi-source BFS才會快, 把所有0的座標放進queue, 
+# 再對這些座標逐步往外擴展, 只要距離比較小就紀錄
 
+from collections import deque
 from typing import List
 
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:        
         rows, cols = len(mat), len(mat[0])
         dist = [[float('inf')] * cols for _ in range(rows)]
-
-        num_rows = len(mat)
-        num_cols = len(mat[0])
-        nearest_zero_map = [[-1 for _ in range(num_cols)] for _ in range(num_rows)]
-        non_zero_positions = []
-        zero_positions = []
-
-        for r_index in range(num_rows):
-            for c_index in range(num_cols):
-                cell = mat[r_index][c_index]
-                if cell == 0:
-                    nearest_zero_map[r_index][c_index] = 0
-                    zero_positions.append([r_index, c_index])
-                    if r_index > 0:
-                        if mat[r_index-1][c_index] != 0:
-                            nearest_zero_map[r_index-1][c_index] = 1                            
-                    if r_index < num_rows-1:
-                        if mat[r_index+1][c_index] != 0:
-                            nearest_zero_map[r_index+1][c_index] = 1                            
-                    if c_index > 0:
-                        if mat[r_index][c_index-1] != 0:
-                            nearest_zero_map[r_index][c_index-1] = 1                            
-                    if c_index < num_cols-1:
-                        if mat[r_index][c_index+1] != 0:
-                            nearest_zero_map[r_index][c_index+1] = 1                           
-
-                else:
-                    if nearest_zero_map[r_index][c_index] == 1:
-                        continue
-                    if r_index > 0:
-                        if mat[r_index-1][c_index] == 0:
-                            nearest_zero_map[r_index][c_index] = 1
-                            continue
-                    if r_index < num_rows-1:
-                        if mat[r_index+1][c_index] == 0:
-                            nearest_zero_map[r_index][c_index] = 1
-                            continue
-                    if c_index > 0:
-                        if mat[r_index][c_index-1] == 0:
-                            nearest_zero_map[r_index][c_index] = 1
-                            continue
-                    if c_index < num_cols-1:
-                        if mat[r_index][c_index+1] == 0:
-                            nearest_zero_map[r_index][c_index] = 1
-                            continue
-                    non_zero_positions.append([r_index, c_index])                                      
+        queue = deque()
         
-        for zero in zero_positions:
-            zero_r = zero[0]
-            zero_c = zero[1]
-            for non_zero in non_zero_positions:
-                non_zero_r = non_zero[0]
-                non_zero_c = non_zero[1]
-                distance = abs(non_zero_r-zero_r)+abs(non_zero_c-zero_c)
-                non_zero_value = nearest_zero_map[non_zero_r][non_zero_c]
-                if non_zero_value == -1 or non_zero_value > distance:
-                    nearest_zero_map[non_zero_r][non_zero_c] = distance
-        
-        return nearest_zero_map
+        for r in range(rows):
+            for c in range(cols):
+                if mat[r][c] == 0:
+                    dist[r][c] = 0
+                    queue.append((r,c))
+
+        directions = [(-1,0),(1,0),(0,-1),(0,1)]
+
+        while queue:
+            r, c = queue.popleft()
+            for direction_r, direction_c in directions:
+                step_r, step_c = r+direction_r, c+direction_c
+                if 0<=step_r<rows and 0<=step_c<cols:
+                    if dist[step_r][step_c] > dist[r][c]+1:
+                        dist[step_r][step_c] = dist[r][c]+1
+                        queue.append((step_r,step_c))
+        return dist
 
 def main():
     mat = [[0,0,0],[0,1,0],[0,0,0]]
