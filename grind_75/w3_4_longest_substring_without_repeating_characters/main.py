148555ba5e223c5afb6dad0c72cdbbdc09b6993b
Tommy12.Lin <tommy12.lin@advantech.com.tw>
2025-04-15 10:03:29 +0800

    grind 75, complete longest substring without repeating characters

--- grind_75/w3_4_longest_substring_without_repeating_characters/main.py
+++ grind_75/w3_4_longest_substring_without_repeating_characters/main.py
@@ -1,20 +1,33 @@
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        longest_len = 0
-        char_set = set()
-        
-        for i in range(len(s)):
-            c = s[i]
-            if c not in char_set:
-                char_set.add(c)
-            else:
-                if len(char_set) > longest_len:
-                    longest_len = len(char_set)                
-                char_set.clear()
-                char_set.add(c)
+# Sliding window + HashSet
+# 使用left, right指標與set
+# 每輪right+1, 當substring沒有重複max(longest_len, right-left+1)
+# 當發現重複字元, left持續+1, 直到找到當下right指到的字元, 過程中持續從set中丟出字元
 
-        if len(char_set) > longest_len:
-            longest_len = len(char_set)  
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:   
+        if len(s)==0: return 0
+        if len(s)==1: return 1
+
+        char_set = set()
+        char_set.add(s[0])
+        longest_len = 1
+        left, right = 0, 1
+                
+        while right < len(s):
+            c = s[right]
+            if c not in char_set:
+                longest_len = max(longest_len, right-left+1)                
+            else:
+                while True:
+                    left_c = s[left]
+                    char_set.remove(left_c)
+                    left+=1
+                    if left_c == c:   
+                        break
+            
+            char_set.add(c)        
+            right+=1
+
         return longest_len    
 
 def main():
