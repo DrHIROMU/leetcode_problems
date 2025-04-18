eb92ea14b18795c9df5cc6ab8cad4d4e47dedbb5
Tommy12.Lin <tommy12.lin@advantech.com.tw>
2025-04-18 13:28:47 +0800

    grind 75, init 3 sum

---
+++ grind_75/w3_5_3_sum/main.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        result: List[List[int]] = []
+        return result
+        
+def main():
+    nums = [-1,0,1,2,-1,-4]
+    solution = Solution()
+    result = solution.threeSum()
+
+if __name__ == '__main__':
+    main()
